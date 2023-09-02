9f2e6377f9999052cbd9a9e45a909c3be485e6c6
Bianca Iordache <biannca.iordache@gmail.com>
2023-09-02 20:58:11 +0100

    Add navbar component

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
@@ -68,3 +69,6 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+=======
+# jobboard-frontend
+>>>>>>> ac9cc2787da31c48739e6e5afea02176c180646f
--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import JobList from './components/JobList';
 import JobDetails from './components/JobDetails';
 import Header from './components/Header';
+import './App.css';
+import { Provider } from 'react-redux';
+import store from './store'; // import your Redux store
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<JobList />} />
-        <Route path="/jobs/:id" element={<JobDetails />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<JobList />} />
+          <Route path="/jobs/:id" element={<JobDetails />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
